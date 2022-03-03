e9ae94dc6281f2bdfe8382ae2471f2cf385754ca
standfer <vertigl@yandex.ru>
2022-03-03 19:01:47 +0800

    add random

--- Example_004_Div/Program.cs
+++ Example_004_Div/Program.cs
@@ -1,3 +1,7 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); // 1 2 3 4..... 9
+Console.Write("Первое случайное число ");
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10); 
+Console.Write("Второе случайное число ");
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
