955ed9bcca2480b7e7c83e17210f73dac09352e5
standfer <vertigl@yandex.ru>
2022-03-03 19:18:13 +0800

    add example 005 Hello if else

---
+++ Example_005_Hello_if/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "max")
+{
+    Console.Write("Привет хозяин");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
